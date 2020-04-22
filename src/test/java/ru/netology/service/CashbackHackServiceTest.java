74cba5f9ccb044f5011cfe1389785215406ae3ba
Julia Naimovich <naimovich_julia@mail.ru>
2020-04-22 19:15:26 +0300

    Parametrized tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,36 +3,20 @@ package ru.netology.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    @DisplayName("Should return 100 if amount is 900")
-    void shouldReturn100IfAmountIs900() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackHackData.csv", numLinesToSkip = 1)
+    @DisplayName("Should calculate missing amount")
+    void shouldCalculateMissingAmount(int amount, int expectedResult) {
     CashbackHackService cashbackHackService = new CashbackHackService();
-    int expectedResult = 100;
-    int actualResult = cashbackHackService.remain(900);
+    int actualResult = cashbackHackService.remain(amount);
     assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Should return 5 if amount is 995")
-    void shouldReturn5IfAmountIs995() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int expectedResult = 5;
-        int actualResult = cashbackHackService.remain(995);
-        assertEquals(expectedResult, actualResult);
-    }
-
-    @Test
-    @DisplayName("Should return 0 if amount is 1000")
-    void shouldReturnZeroIfAmountIs1000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int expectedResult = 0;
-        int actualResult = cashbackHackService.remain(1000);
-        assertEquals(expectedResult, actualResult);
     }
 
     @Test
